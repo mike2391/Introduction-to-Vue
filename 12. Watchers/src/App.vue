91d7f45ae5999c139e654d6475fef0d2fe06ae0c
mike2391 <michael2391valdio@gmail.com>
2026-05-12 23:56:57 +0700

    watchers undone

--- 12. Watchers/src/App.vue
+++ 12. Watchers/src/App.vue
@@ -1,11 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from "vue";
+
+const productID = ref("");
+const product = ref(null);
+
+watch(productID, async (newVal, oldVal) => {
+  if (newVal) {
+    const response = await fetch(`/product${newVal}.json`);
+    product.value = await response.json();
+  } else {
+    product.value = null;
+  }
+});
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <label for="productID">
+    Product Id:
+    <select v-model="productID">
+      <option value=""></option>
+      <option value="product1">Product 1</option>
+      <option value="product2">Product 2</option>
+      <option value="product3">Product 3</option>
+    </select>
+  </label>
+
+  <div v-if="product">
+    <h1>Product</h1>
+    <p>ID: {{ product.id }}</p>
+    <p>Name: {{ product.name }}</p>
+    <p>Price: ${{ product.price.toFixed(2) }}</p>
+  </div>
 </template>
 
 <style scoped></style>
