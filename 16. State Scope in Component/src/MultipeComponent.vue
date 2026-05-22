a0ef1e92ecbb2242329214522a7a122d63b8c01e
mike2391 <michael2391valdio@gmail.com>
2026-05-23 00:01:48 +0700

    finishing state scope in component

--- 16. State Scope in Component/src/MultipeComponent.vue
+++ 16. State Scope in Component/src/MultipeComponent.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import Counter from "./components/Counter.vue";
+</script>
 
+<!-- In this session i will talk about state scope in multiple components -->
+<!-- Simple, every state in each component is unique -->
+<!-- So eventho they have the same name, each component has it's own state -->
+
+<!-- You can see the example in component "Counter" -->
+<!-- Counter 1 and Counter 2 have their own state, so eventho you click the button in Counter 1, it will not affect the state of Counter 2 -->
 <template>
-  
+  <h1>Counter 1</h1>
+  <Counter />
+  <h1>Counter 2</h1>
+  <Counter />
 </template>
 
 <style scoped></style>
---
+++ 16. State Scope in Component/src/components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</template>
+
+<style scoped></style>
