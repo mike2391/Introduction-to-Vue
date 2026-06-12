7f0512cf19746f1d5081eb08552ccbaf43738ffb
mike2391 <michael2391valdio@gmail.com>
2026-06-12 23:58:55 +0700

    add component event (undone)

---
+++ 18. Component Event/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import Counter from "./Counter.vue";
+
+const counter = ref(0);
+
+function incrementCounter(value) {
+  counter.value += value;
+}
+</script>
+
+<template>
+  <Counter name="Counter 1" :counter="counter" :increment="1" @click="incrementCounter(1)" />
+  <Counter name="Counter 2" :counter="counter" :increment="2" @click="incrementCounter(2)" />
+</template>
+
+<style scoped></style>
---
+++ 18. Component Event/src/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { counter, name, increment } = defineProps({
+  counter: Number,
+  name: String,
+  increment: Number,
+});
+
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+  <h1>Counter: {{ counter }} - Name: {{ name }}</h1>
+  <button @click="emit('click', increment)">increment {{ name }}</button>
+</template>
+
+<style scoped></style>
